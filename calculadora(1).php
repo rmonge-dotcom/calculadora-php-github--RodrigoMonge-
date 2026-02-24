066c4fe42f2bb932215b36e8a1b648bd1e649a7c
Dante <dante@tu-correo.com>
2026-02-24 17:36:01 +0100

    Substituïda lògica if/else per switch a la funció calcular

--- calculadora(1).php
+++ calculadora(1).php
@@ -1,21 +1,32 @@
 <?php
-$op = $_POST['op'];
-$n1 = $_POST['n1'];
-$n2 = $_POST['n2'];
 
-if ($op == "s") {
-    $res = $n1 + $n2;
-} else if ($op == "r") {
-    $res = $n1 - $n2;
-} else if ($op == "m") {
-    $res = $n1 * $n2;
-} else if ($op == "d") {
-    if ($n2 != 0) {
-        $res = $n1 / $n2;
-    } else {
-        $res = "Error";
+function calcular($operacio, $a, $b) {
+    switch ($operacio) {
+        case "s":
+            return $a + $b;
+
+        case "r":
+            return $a - $b;
+
+        case "m":
+            return $a * $b;
+
+        case "d":
+            if ($b != 0) {
+                return $a / $b;
+            } else {
+                return "Error";
+            }
+
+        default:
+            return "Error";
     }
 }
 
-echo $res;
+$operacio = $_POST['op'];
+$primerNumero = $_POST['n1'];
+$segonNumero = $_POST['n2'];
+
+echo calcular($operacio, $primerNumero, $segonNumero);
+
 ?>
